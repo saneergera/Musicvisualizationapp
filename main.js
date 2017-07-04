4e14924ca564d0a8744f1a6163b0a5b1eb6d6093
Saneer <saneergera@gmail.com>
2017-07-05 02:35:41 +0530

    Made the contols work

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+//common variable description
+
+var Playingnumber = 0  ;
+var shuffle=0;
+
+
+
+
 function fancyTimeFormat(time) {
     // Hours, minutes and seconds
     var hrs = ~~(time / 3600);
@@ -134,6 +142,7 @@ $(".player-progress").click(function(event) {
 
 var song = document.querySelector('audio');
 song.currentTime = (song.duration*calc)/100;
+
 updateTimer();
 
 
@@ -161,10 +170,12 @@ function changeCurrentSongDetails(songObj) //function creat kiya ek argument pas
 
 function addSongNameClickEvent(songObj, position) //we have made a machine jispe 2 buttons diye hai songName and position ke liye
 {
+
     var songName = songObj.fileName;
     var id = '#song' + position; //#song ke saath position ko jod do and agar position 1 hai to output #song1 hogi jisse id mein store kar diya
     $(id).click(function() //agar #song1 hai to one vaale div pe event lage ga
         {
+              Playingnumber= (position - 1)
             var audio = document.querySelector('audio');
             var currentSong = audio.src;
             if (currentSong.search(songName) != -1) {
@@ -177,6 +188,15 @@ function addSongNameClickEvent(songObj, position) //we have made a machine jispe
         });
 }
 
+
+function changeSong() //we have made a machine jispe 2 buttons diye hai songName and position ke liye
+{
+var music =  songs[Playingnumber].fileName;
+var song = document.querySelector("audio");
+song.src = music;
+toggleSong();
+changeCurrentSongDetails(songs[Playingnumber])
+}
 window.onload = function() {
 
 
@@ -212,6 +232,162 @@ window.onload = function() {
 }
 
 
+$(".fa-step-forward").click(function(){
+
+if(Playingnumber == songs.length-1){
+console.log("one");
+Playingnumber = 0;
+changeSong();
+
+
+
+}
+
+else {
+console.log("two");
+console.log(Playingnumber);
+  Playingnumber++;
+changeSong();
+}
+
+
+
+
+})
+
+
+
+
+$(".fa-step-backward").click(function(){
+
+if(Playingnumber == 0){
+console.log("one");
+Playingnumber = (songs.length-1);
+changeSong();
+
+
+
+
+}
+
+else {
+console.log("two");
+console.log(Playingnumber);
+  Playingnumber--;
+changeSong();
+}
+
+
+
+
+})
+
+function shufflee(a) {
+    var j, x, i;
+    for (i = a.length; i; i--) {
+        j = Math.floor(Math.random() * i);
+        x = a[i - 1];
+        a[i - 1] = a[j];
+        a[j] = x;
+    }
+}
+
+
+
+$(".fa-random").click(function(){
+
+
+$(this).toggleClass("active");
+
+
+if(shuffle==0)
+{
+shuffle = 1;
+shufflee(songs);
+changeCurrentSongDetails(songs[0]);
+song =document.querySelector("audio");
+song.src = songs[0].fileName;
+toggleSong();
+Playingnumber=0;
+
+    for (var i = 0; i < songs.length; i++) //Var i zero se 3 tak chalana hai
+
+    {
+        var obj = songs[i]; //Diary ke andar se page utha ke humne obj variable mein store kar diya
+        var name = '#song' + (i + 1);
+        var song = $(name);
+        song.find('.song-name').text(obj.name); //("song1 .songname")
+        song.find('.song-artist').text(obj.artist);
+        song.find('.song-album').text(obj.album);
+        song.find('.song-length').text(obj.duration);
+        addSongNameClickEvent(obj, i + 1);
+    }
+}
+
+else {
+  shuffle = 0;
+
+   songs = [{
+          'name': 'Badri Ki Dulhania (Title Track)',
+          'artist': 'Neha Kakkar, Monali Thakur, Ikka Singh, Dev Negi',
+          'album': 'Badrinath ki Dulhania',
+          'duration': '2:56',
+          'fileName': 'song1.mp3',
+          'image': 'song1.jpg'
+      },
+      {
+          'name': 'Humma Song',
+          'artist': 'Badshah, Jubin Nautiyal, Shashaa Tirupati',
+          'album': 'Ok Jaanu',
+          'duration': '3:15',
+          'fileName': 'song2.mp3',
+          'image': 'song2.jpg'
+      },
+      {
+          'name': 'Nashe Si Chadh Gayi',
+          'artist': 'Arijit Singh',
+          'album': 'Befikre',
+          'duration': '2:34',
+          'fileName': 'song3.mp3',
+          'image': 'song3.jpg'
+      },
+      {
+          'name': 'The Breakup Song',
+          'artist': 'Nakash Aziz, Arijit Singh, Badshah, Jonita Gandhi',
+          'album': 'Ae Dil Hai Mushkil',
+          'duration': '2:29',
+          'fileName': 'song4.mp3',
+          'image': 'song4.jpg'
+      }
+  ]
+
+  changeCurrentSongDetails(songs[0]);
+  song =document.querySelector("audio");
+  song.src = songs[0].fileName;
+toggleSong();
+Playingnumber=0;
+  for (var i = 0; i < songs.length; i++) //Var i zero se 3 tak chalana hai
+
+  {
+      var obj = songs[i]; //Diary ke andar se page utha ke humne obj variable mein store kar diya
+      var name = '#song' + (i + 1);
+      var song = $(name);
+      song.find('.song-name').text(obj.name); //("song1 .songname")
+      song.find('.song-artist').text(obj.artist);
+      song.find('.song-album').text(obj.album);
+      song.find('.song-length').text(obj.duration);
+      addSongNameClickEvent(obj, i + 1);
+  }
+
+
+}
+
+
+
+
+
+
+})
 
 //   addSongNameClickEvent(fileNames[0],1);
 // addSongNameClickEvent(fileNames[1],2);
